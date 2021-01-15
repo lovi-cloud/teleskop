044deded8f9901f536a5b880c4e5c0e785457023
whywaita <whywaita@whywrite.it>
2021-01-15 15:49:15 +0900

    use single path when target ip is only one

--- block_device.go
+++ block_device.go
@@ -11,9 +11,20 @@ import (
 )
 
 func (a *agent) ConnectBlockDevice(ctx context.Context, req *pb.ConnectBlockDeviceRequest) (*pb.ConnectBlockDeviceResponse, error) {
-	deviceName, err := osbrick.ConnectMultipathVolume(ctx, req.PortalAddresses, int(req.HostLunId))
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to connect block device: %+v", err)
+	var deviceName string
+	var err error
+
+	switch len(req.PortalAddresses) {
+	case 1:
+		deviceName, err = osbrick.ConnectSinglePathVolume(ctx, req.PortalAddresses[0], int(req.HostLunId))
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to connect block device: %+v", err)
+		}
+	default:
+		deviceName, err = osbrick.ConnectMultipathVolume(ctx, req.PortalAddresses, int(req.HostLunId))
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to connect block device: %+v", err)
+		}
 	}
 
 	return &pb.ConnectBlockDeviceResponse{
@@ -22,8 +33,15 @@ func (a *agent) ConnectBlockDevice(ctx context.Context, req *pb.ConnectBlockDevi
 }
 
 func (a *agent) DisconnectBlockDevice(ctx context.Context, req *pb.DisconnectBlockDeviceRequest) (*pb.DisconnectBlockDeviceResponse, error) {
-	if err := osbrick.DisconnectVolume(ctx, req.PortalAddresses, int(req.HostLunId)); err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to disconnect block device :%+v", err)
+	switch len(req.PortalAddresses) {
+	case 1:
+		if err := osbrick.DisconnectSinglePathVolume(ctx, req.PortalAddresses[0], int(req.HostLunId)); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to disconnect block device: %+v", err)
+		}
+	default:
+		if err := osbrick.DisconnectVolume(ctx, req.PortalAddresses, int(req.HostLunId)); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to disconnect block device: %+v", err)
+		}
 	}
 
 	return &pb.DisconnectBlockDeviceResponse{}, nil
